337f14904763bc4ed74c0ae60e64dc1cec6a5245
admin <admin@iMac3-2.local>
2018-04-20 11:12:03 +0200

    JS finished, CSS in progress

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const apiKey = "RGAPI-3f9ce90d-a226-45c4-9c51-2ac5875db720";
+const apiKey = "RGAPI-cbe0a889-18ae-445a-8f5c-7137d4885f50";
 
 $(document).ready(function() {
   if (localStorage.getItem('championList') == null) {
     $.ajax({
-      url: "https://euw1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false",
+      url: "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false",
       type: "GET",
       headers: {
         "X-Riot-Token": apiKey,
@@ -21,10 +21,15 @@ $(document).ready(function() {
 
   //The button A shows a list of every champions
   $('#a').on('click', function (e) {
+    if ($('.champ-list')!= null) {
+      $('.champ-list').remove();
+      $('.champ-infos').remove();
+    }
     const response = dataParseKey('championList');
     const list = document.createElement('ul');
     const objChamp = response.data;
-    const arrChamp = Object.keys(response.data);
+    const arrChamp = Object.keys(response.data).sort();
+    list.classList.add('champ-list');
 
     for (let i = 0; i < arrChamp.length; i++) {
       const listChampion = document.createElement('li');
@@ -39,69 +44,103 @@ $(document).ready(function() {
     $('.champion').on('click', function (e) {
       const champId = this.dataset.id;
       $.ajax({
-        url: "https://euw1.api.riotgames.com/lol/static-data/v3/champions/" + champId + "?locale=en_US&champData=spells&tags=all",
+        url: "https://na1.api.riotgames.com/lol/static-data/v3/champions/" + champId + "?locale=en_US&champData=spells&tags=all",
         type: "GET",
         headers: {
           "X-Riot-Token": apiKey,
         },
         success: function (response) {
+          if ($('.champ-infos')!= null) {
+            $('.champ-infos').remove();
+          }
           //To have a div that contains all the infos
           const champContainer = document.createElement('div');
           const headerContainer = document.createElement('div');
+          const titleDiv = document.createElement('div');
+          const mainContainer = document.createElement('div');
           champContainer.classList.add('champ-infos');
-          headerContainer.classList.add('header-infos')
-
+          headerContainer.classList.add('header-infos');
+          mainContainer.classList.add('spell-infos');
+          titleDiv.classList.add('title-div');
 
           //To have the name
           const name = response.name;
           const nameTitle = document.createElement('h2');
+          nameTitle.classList.add('title-champ');
           nameTitle.innerHTML = name;
+          titleDiv.appendChild(nameTitle);
 
           //To have the champion's image
+          const avatar = response.image.full;
           const imgChamp = document.createElement('img');
           const imgChampDiv = document.createElement('div');
           imgChamp.classList.add('img-champ');
           imgChampDiv.classList.add('img-champ-div');
-          imgChamp.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + name + ".png";
+          imgChamp.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + avatar;
           imgChampDiv.appendChild(imgChamp);
-          headerContainer.appendChild(imgChampDiv);
+          titleDiv.appendChild(imgChampDiv);
 
           //To have the lore
           const loreParagraph = document.createElement('p');
-          loreParagraph.classList.add('lore-p');
+          loreParagraph.classList.add('champ-p');
           loreParagraph.innerHTML = response.lore;
-          headerContainer.appendChild(loreParagraph);
           
           //To have the passive
-          const passive = response.passive.image;
+          const passive = response.passive.full;
+          const namePassive = response.passive.name;
+          const descPassive = response.passive.description;
           const passiveParagraph = document.createElement('p');
-          const passiveImg = document.createElement('img');
-          const passiveImgDiv = document.createElement('div');
+          const passiveTitle = document.createElement('h4');
+          const passivContainer = document.createElement('div');
+          //const passiveImg = document.createElement('img');
+          //const passiveImgDiv = document.createElement('div');
           passiveParagraph.classList.add('passive-p');
-          passiveParagraph.innerHTML = response.passive.description;
-          passiveImgDiv.classList.add('passive-img-div');
-          passiveImg.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/" + passive + ".png"
+          passiveTitle.classList.add('title-h4');
+          passiveTitle.innerHTML = namePassive;
+          passiveParagraph.innerHTML = descPassive;
+          //passiveImgDiv.classList.add('passive-img-div');
+          //passiveImg.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/passive/" + passive;
+          passivContainer.appendChild(passiveTitle);
+          //passivContainer.appendChild(passiveImgDiv);
+          passivContainer.appendChild(passiveParagraph);
 
           //To have the 4 spells with image, name and description
-          const spells = response.spells;
-          for (let i = 0; i < spells.length; i++) {
-            const nameSpell = spells[i].name;
-            const descSpell = spells[i].description;
+          const tableSpells = response.spells;
+          const spellContainerList = document.createElement('ul');
+          spellContainerList.classList.add('spell-ul');
+          for (let i = 0; i < tableSpells.length; i++) {
+            const nameSpell = tableSpells[i].name;
+            const descSpell = tableSpells[i].description;
+            const spell = tableSpells[i].image.full;
             const nameSpellTitle = document.createElement('h4');
-            const nameSpellParagraph = document.createElement('p');
+            const spellParagraph = document.createElement('p');
             const imgSpell = document.createElement('img');
             const imgSpellDiv = document.createElement('div');
-            imgSpell.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell" + nameSpell + ".png";
-            //append the items
+            const spellList = document.createElement('li');
+            nameSpellTitle.classList.add('title-h4');
+            spellParagraph.classList.add('champ-p');
+            imgSpell.classList.add('img-spell');
+            imgSpellDiv.classList.add('img-spell-div');
+            spellList.classList.add('spell');
+            imgSpell.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/" + spell;
+            nameSpellTitle.innerHTML = nameSpell;
+            spellParagraph.innerHTML = descSpell;
+            imgSpellDiv.appendChild(imgSpell);
+            spellList.appendChild(nameSpellTitle);
+            spellList.appendChild(imgSpellDiv);
+            spellList.appendChild(spellParagraph)
+            spellContainerList.appendChild(spellList);
           }
 
           //Add 2 h3 to separate spells and header
           //To insert in the html document
-          champContainer.appendChild(nameTitle);
+          headerContainer.appendChild(titleDiv);
+          headerContainer.appendChild(loreParagraph);
+          mainContainer.appendChild(passivContainer);
+          mainContainer.appendChild(spellContainerList);
           champContainer.appendChild(headerContainer);
-          champContainer.appendChild(loreParagraph);
+          champContainer.appendChild(mainContainer);
           document.querySelector('#resultList').appendChild(champContainer);
-          debugger;
         }
       });
     });
